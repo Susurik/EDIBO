64fe2a5c4f5805eb5f6e02063260af0d2d6303a5
Susurik <andris.timsans@gmail.com>
2020-07-08 16:32:18 +0300

    Add files via upload

---
+++ Day6_08.07/dd.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+echo "Enter any 3 nr decimal no:"
+read num
+
+dalitais=$(( num/ 2))
+atlikums=$(( num% 2))
+fst=$((num / 2))
+snd=$fst
+trd=$snd
+
+#while [ $num -gt 0 ]; do
+	
+	
+	echo $(( fst / 2 ))
+	echo $(( snd / 2 ))
+	echo $(( trd / 2 ))
+
+
+
+	#exit 1
+#done
