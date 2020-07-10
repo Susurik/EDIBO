098c0c69f98340c086e3d8b861c9ac04da43101c
Andris T <andris.timsans@gmail.com>
2020-07-10 19:06:53 +0300

    Add files via upload

---
+++ Day7_10.07/current_session.py
@@ -0,0 +1,36 @@
+# coding: utf-8
+f = open("a.dat, "w")
+f = open("a.dat", "w")
+.close
+f.close
+f
+f.open
+f
+f.closed
+f
+f = open("a.dat", "w")
+f
+f.closed
+f.tell()
+f.write("Hello")
+f.closed
+f.tell()
+f.closed()
+f.closed
+f.tell()
+f.write("\n\tHelloz\n")
+f.closed
+f.close()
+f.closed
+f = open("a.dat")
+f.tell()
+f.readline()
+f.readline()
+f.readline()
+f.readline()
+f.readline()
+f.readline()
+f.close()
+f.closed
+get_ipython().run_line_magic('history', '')
+get_ipython().run_line_magic('save', 'current_session ~0/')
