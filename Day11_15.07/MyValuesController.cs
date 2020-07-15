552bdfef81482451cf00c54916b7548545061e5d
Andris T <andris.timsans@gmail.com>
2020-07-15 18:50:47 +0300

    Add files via upload

---
+++ Day11_15.07/MyValuesController.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MyFirstWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    
+    public class MyValuesController : ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "July", "2020", "EDIBO", "RTU" };
+        }
+    }
+}
