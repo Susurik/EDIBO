1b385c178de925372f64de5e113f7f3f71d3dc9c
Andris T <andris.timsans@gmail.com>
2020-07-24 19:26:14 +0300

    Add files via upload

---
+++ Day18_24.07/src/app/grid/grid.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grid',
+  templateUrl: './grid.component.html',
+  styleUrls: ['./grid.component.css']
+})
+export class GridComponent implements OnInit {
+  title = 'gridemo';
+  public list = [];
+
+  public ngOnInit() {
+    this.list = [
+      { rows: 1, cols: 1, data: 1 },
+      { rows: 1, cols: 1, data: 2 },
+      { rows: 1, cols: 1, data: 3 },
+      { rows: 1, cols: 1, data: 4 },
+
+      { rows: 1, cols: 1, data: 5 },
+      { rows: 1, cols: 1, data: 6 },
+      { rows: 1, cols: 1, data: 7 },
+      { rows: 1, cols: 1, data: 8 },
+      { rows: 1, cols: 1, data: 9 },
+
+      { rows: 1, cols: 1, data: 10 },
+      { rows: 1, cols: 1, data: 11 },
+      { rows: 1, cols: 1, data: 12 },
+      { rows: 1, cols: 1, data: 13 },
+      { rows: 1, cols: 1, data: 14 },
+
+      { rows: 1, cols: 1, data: 15 },
+      { rows: 1, cols: 1, data: 16 },
+      { rows: 1, cols: 1, data: 17 },
+      { rows: 1, cols: 1, data: 18 },
+      { rows: 1, cols: 1, data: 19 },
+      
+      { rows: 1, cols: 1, data: 20 },
+      { rows: 1, cols: 1, data: 21 },
+      { rows: 1, cols: 1, data: 22 },
+      { rows: 1, cols: 1, data: 23 },
+      { rows: 1, cols: 1, data: 24 },
+
+      { rows: 1, cols: 1, data: 25 },
+      { rows: 1, cols: 1, data: 26 },
+      { rows: 1, cols: 1, data: 27 },
+      { rows: 1, cols: 1, data: 28 },
+      { rows: 1, cols: 1, data: 29 },
+
+      { rows: 1, cols: 1, data: 30 },
+      { rows: 1, cols: 1, data: 31 },
+      { rows: 1, cols: 1, data: 32 },
+      
+    ];
+  }
+}
