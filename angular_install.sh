ba1169c547a9285f548c0e7376e5c3d02a75b81e
Andris T <andris.timsans@gmail.com>
2020-07-27 22:19:32 +0300

    Update angular_install.sh

--- angular_install.sh
+++ angular_install.sh
@@ -1,29 +1,40 @@
+echo "CD home"
+
+cd
+
+echo "Installing NVM"
+
 export NVM_DIR="$HOME/.nvm" && (
  git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
  cd "$NVM_DIR"
  git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
 ) && \. "$NVM_DIR/nvm.sh"
-echo "1st done"
 
 nvm i 12
 
-echo "2nd done"
+echo "Loading NVM"
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-
-echo "3rd done"
 
 echo "Installing angular"
 
 npm install -g @angular/cli
 
+echo "CD home"
+
 cd
 
+echo "Creating new project EDIBO"
+
 ng new EDIBO
+
+echo "CD EDIBO"
 
 cd EDIBO
 
 echo "installing angular material"
 
 ng add @angular/material
+
+echo "ALL DONE!"
